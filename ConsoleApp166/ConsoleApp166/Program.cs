fe4d9d57a10e65007c454729e7349e970be1e38f
daconiki <133904497+daconiki@users.noreply.github.com>
2024-06-05 13:00:19 +0300

    zad1

---
+++ ConsoleApp166/ConsoleApp166/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете n за елементи");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Въведете елемент");
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            int sum = 0;
+            int min = arr[0];
+            int max = arr[0];
+            int avr = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+
+                if (min > arr[i]) min = arr[i];
+                if (max < arr[i]) max = arr[i];
+                avr = sum / n;
+            }
+
+            Console.WriteLine("Сумата на масива е "+sum);
+            Console.WriteLine("Минималният елементе е "+min);
+            Console.WriteLine("Максималният елемент е "+max);
+            Console.WriteLine("Средното аритметично е "+avr);
+        }
+    }
+}
